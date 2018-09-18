bd2b4a77d30b29005157a10c43c597eb68ef50aa
Taehoon Kim <cearial09@gmail.com>
2018-09-18 19:16:45 +0900

    Modify script

--- trace_gen.sh
+++ trace_gen.sh
@@ -3,5 +3,5 @@
 ## usage
 ## bash trace_gen.sh [workload_name] <-- a,b,c ...
 
-bin/ycsb load kvtracer -P workloads/workload$1 -p "kvtracer.tracefile=tracea_load.txt" -p "kvtracer.keymapfile=tracea_keys.txt"
-bin/ycsb run kvtracer -P workloads/workload$1 -p "kvtracer.tracefile=tracea_run.txt" -p "kvtracer.keymapfile=tracea_keys.txt"
+bin/ycsb load kvtracer -P workloads/workload$1 -p "kvtracer.tracefile=tracea_load_$1.txt" -p "kvtracer.keymapfile=tracea_keys_$1.txt"
+bin/ycsb run kvtracer -P workloads/workload$1 -p "kvtracer.tracefile=tracea_run_$1.txt" -p "kvtracer.keymapfile=tracea_keys_$1.txt"
