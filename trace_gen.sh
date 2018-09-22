f989ad462b618a01a58be06a9d54b91c6161750b
Taehoon Kim <cearial09@gmail.com>
2018-09-22 16:57:48 +0900

    Fix minor bug on script

--- trace_gen.sh
+++ trace_gen.sh
@@ -5,9 +5,9 @@
 
 for j in {a,b,c,d,f,g,h,i} 
 do
-	bin/ycsb load kvtracer -P workloads/workload$1 -p "kvtracer.tracefile=tracea_load_$j.txt" -p "kvtracer.keymapfile=tracea_keys_$j.txt"
+	bin/ycsb load kvtracer -P workloads/workload$j -p "kvtracer.tracefile=tracea_load_$j.txt" -p "kvtracer.keymapfile=tracea_keys_$j.txt"
 	for i in {0..8}
 	do		
-		bin/ycsb run kvtracer -P workloads/workload$1 -p "kvtracer.tracefile=tracea_run_$j.$i.txt" -p "kvtracer.keymapfile=tracea_keys_$j.txt"
+		bin/ycsb run kvtracer -P workloads/workload$j -p "kvtracer.tracefile=tracea_run_$j.$i.txt" -p "kvtracer.keymapfile=tracea_keys_$j.txt"
 	done
 done
