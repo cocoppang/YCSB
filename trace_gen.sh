cccf64107deea186ef296780793a8350422dd7d1
Taehoon Kim <cearial09@gmail.com>
2018-09-22 16:46:20 +0900

    Change workload configuration by default

--- trace_gen.sh
+++ trace_gen.sh
@@ -3,5 +3,11 @@
 ## usage
 ## bash trace_gen.sh [workload_name] <-- a,b,c ...
 
-bin/ycsb load kvtracer -P workloads/workload$1 -p "kvtracer.tracefile=tracea_load_$1.txt" -p "kvtracer.keymapfile=tracea_keys_$1.txt"
-bin/ycsb run kvtracer -P workloads/workload$1 -p "kvtracer.tracefile=tracea_run_$1.txt" -p "kvtracer.keymapfile=tracea_keys_$1.txt"
+for j in {a,b,c,d,f,g,h,i} 
+do
+	bin/ycsb load kvtracer -P workloads/workload$1 -p "kvtracer.tracefile=tracea_load_$j.txt" -p "kvtracer.keymapfile=tracea_keys_$j.txt"
+	for i in {0..8}
+	do		
+		bin/ycsb run kvtracer -P workloads/workload$1 -p "kvtracer.tracefile=tracea_run_$j.$i.txt" -p "kvtracer.keymapfile=tracea_keys_$j.txt"
+	done
+done
